4695e1507c90263c296fdc0d603134ca6b2f6936
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2022-12-04 12:44:54 +0500

    Yechim yangilandi.

--- get_post_month.py
+++ get_post_month.py
@@ -1,3 +1,4 @@
+import datetime
 from read_data import fromJson
 
 # Define a function get post for given month
@@ -16,7 +17,8 @@ def get_post_month(data: dict, month: int) -> int:
     # Loop through the dictionary
     for e in data["messages"]:
         type_ = e["type"]
-        month_ = int(e["date"][5:7])
+        date_ = datetime.datetime.strptime(e["date"], "%Y-%m-%dT%H:%M:%S")
+        month_ = date_.month
         if month_ == month and type_ == "message":
             count += 1
     return count
@@ -28,3 +30,4 @@ data = fromJson(file_path)
 # Get the number of posts for the month of September
 count = get_post_month(data, 10)
 print(count)
+# Output: 334
